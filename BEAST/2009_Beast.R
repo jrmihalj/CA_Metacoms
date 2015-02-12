21c3159eca49c4cb1d3de745e5604de02ffb943d
Joseph R. Mihaljevic <joseph.mihaljevic@colorado.edu>
2015-02-12 17:52:46 -0600

    I had to code this so that you run JAGS via the shell (i.e. system() ). Just like my OccMod_EMS project.

--- BEAST/2009_Beast.R
+++ BEAST/2009_Beast.R
@@ -43,42 +43,56 @@ library(rjags)
 library(random)
 load.module("lecuyer")
 load.module("glm")
-# data:
-jags_d <- list(Y=Y_2009,
-               X=X_2009,
-               Species=Species_2009,
-               Nspecies=Nspecies_2009,
-               Ncov=Ncov_2009,
-               Nobs=Nobs_2009,
-               J=J_2009)
 
-# parameters:
-params <- c("alpha", "betas", "p.detect", 
-            "p.include", "sd.beta.post", "mean.beta.post",
-            "z")
+# Input the data:
+Y=Y_2009
+X=X_2009
+Species=Species_2009
+Nspecies=Nspecies_2009
+Ncov=Ncov_2009
+Nobs=Nobs_2009
+J=J_2009
 
-jinits <- function() {
-  list(
-    z=ifelse(Y_2009 > 0, 1, 0),
-    .RNG.name=c("base::Super-Duper"),
-    .RNG.seed=as.numeric(randomNumbers(n = 1, min = 1, max = 1e+06, col=1))
-  )
-}
+setwd("/home/jmihaljevic/PhD/CAmetacoms/2009/")
 
-JAGS_func <- function(store, adapt, burn, thin){
-  mod <- jags.model(file = "MLMmodel2009_all.txt",
-                    data = jags_d, n.chains = 1, n.adapt=adapt,
-                    inits = jinits)
-  update(mod, n.iter=burn)
-  out <- coda.samples(mod, n.iter = store*thin, variable.names = params, thin=thin)
-  return(out)
-}
-
-#Change wd so that JAGS can find model spec
-setwd("/home/jmihaljevic/PhD/CAmetacoms/2009")
-
-jags.parsamps <- NULL
-jags.parsamps <- JAGS_func(store=100, adapt=100, burn=5, thin=5)
-
+# create unique identifier for iteration
+uniqueID <- paste(i, "-", runif(1, 0, 1), sep="")
 pid <- Sys.getpid()
-save(jags.parsamps, file=paste("2009Chain", pid, sep="_"))
+# make a data file with unique name
+dump(c("X", "Y", "Species", "Nspecies", "Ncov", "Nobs", "J"), 
+     file=paste(uniqueID, "data.R", sep="_"))
+
+# Set initial parameters:
+# Z values (unobserved)
+zinit <- NULL
+zinit <- ifelse(Y_2009 > 0, 1, 0)
+z = zinit
+
+# Write a file of the inits:
+dump("z", file=paste(uniqueID, "inits.R", sep="_"))
+
+# write a unique jags.cmd file that refers to the data and initial values
+cat(paste('model in "MLMmodel2009_all.txt"
+          data in "', uniqueID, '_data.R"
+          compile, nchains(1)
+          parameters in "', uniqueID, '_inits.R"
+          initialize
+          adapt 100
+          update 10
+          monitor alpha, thin(10)
+          monitor betas, thin(10)
+          monitor p.detect, thin(10)
+          monitor sd.beta.post, thin(10)
+          monitor mean.beta.post, thin(10)
+          monitor z, thin(10)
+          update 10
+          coda *, stem(PID', pid, ')
+          exit', 
+          sep=""), 
+    file=paste(uniqueID, "jags.cmd", sep=""))
+
+# Run the JAGS script file:
+system(paste("jags ", uniqueID, "jags.cmd", sep=""))
+
+# Delete all newly created files and directory:
+system(paste("rm ", uniqueID, "*", sep=""))
